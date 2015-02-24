63821f53639194a9b7a53331eb633a39dbb21a3f
Christopher Selden & Kevin Alwell <pair+chrisseldo.kevalwell@devbootcamp.com>
2015-02-23 19:39:46 -0500

    Finished Puttin Orb in base of Staff

---
+++ app/controllers/index.rb
@@ -0,0 +1,33 @@
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+get '/applesauce' do
+  erb :apple_sauce
+end
+
+get '/creamcheese' do
+  erb :creamcheese
+end
+
+get '/bodybuilding' do
+  erb :bodybuilding
+end
+
+get '/form' do
+  puts
+   @message = params["message"]
+  erb :form
+
+end
+
+get '/:name' do
+ @name = params[:name]
+ erb :name
+end
+
+
+
+

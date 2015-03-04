76c12ffa64394905accad193f98b129c5a8c0867
Kevin Alwell <kevalwell@gmail.com>
2015-03-03 19:48:59 -0500

    Partially Finished

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,20 @@ get '/' do
   erb :index
 end
 
+get '/.json' do
+  #content_type: json
+  p params
+  if params[:code] == "1234"
+  {secret_user: 'East Philosophy'}.to_json
+else
+  403
+  end
+end
+
+get '/guess' do
+  erb :guess
+end
+
+post '/' do
+
+end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,21 +1,20 @@
-var calculator = {
-  add: function (x,y){
-    return x + y;
-  },
+$(document).ready(function() {
+  $('#guess').click(function(){
+    guess = prompt("Guess the secret code to reveal a json object in the javascript console.");
+    $.ajax({
+      url: "/.json",
+      type: "get",
+      data: {code: guess},
+      success: function(callback){
+        console.log(callback);
+      },
+       error: function(callback){
+        console.log(callback.status);
+      }
+    })
 
-  subtract: function(x,y) {
-    return x - y;
-  },
 
-  multiply: function(x,y) {
-    return x * y;
-  },
-  divide: function(x,y) {
-    if (y==0) {
-      return "Do not divide by zero."
-    }
-    else {
-      return x / y;
-    }
-  }
-};
+  });
+});
+
+

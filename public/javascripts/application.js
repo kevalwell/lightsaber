e6688b13e99a343f2c6dcc37b4011aad4d3bb176
Kevin Alwell <kevalwell@gmail.com>
2015-03-06 08:42:28 -0500

    Finished challenge

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,40 +1,19 @@
 $(document).ready(function() {
-  $('#guess').click(function(){
-    guess = prompt("Guess the secret code to reveal a json object in the javascript console.");
-    $.ajax({
-      url: "/.json",
-      type: "get",
-      data: {code: guess},
-      success: function(callback){
-        console.log(callback);
-      },
-      error: function(callback){
-        console.log(callback.status);
-      }
-    })
+  var showyaself = function() {
 
+    var thing = "cheese";
+    var thing1 = "applesauce";
 
-  });
+    var showing = function(type) {
+      return "this is a " + type + " Thing";
+    };
 
-  $('#loginbutton').click(function(e){
-    e.preventDefault();
-    data = $('this').closest('form').serialize();
-    console.log(data);
-    $.ajax({
-      url: "/login.json",
-      type: "post",
-      data: data,
-      success: function(callback){
-        console.log(callback);
-        $('.container').replaceWith('<div>Hi '+callback.username+' </div>');
-      },
-      error: function(callback){
-        console.log(callback.status);
-      }
-    })
+    var coat = function(attribute) {
+      return "there is a " + attribute;
+    };
+    return {showing: showing, coat: coat};
+    }();
 
-
-
-
-  });
-});
+    console.log(showyaself.thing);
+    console.log(showyaself.showing("house"));
+})
